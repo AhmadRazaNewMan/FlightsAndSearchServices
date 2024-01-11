22393316461b1e31a73b2df0a35d5fa55590bd7f
AhmadRazaNewMan <webdevolopmentbyar@gmail.com>
2024-01-11 21:59:16 +0500

    third commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+const express  = require("express")
+
+const startAndRundServer = () =>{
+    const app = express()
+    const PORT = 3000;
+
+    app.listen(PORT,()=>{
+
+        console.log(`App is running at ${PORT}`)
+
+    })
+}
+startAndRundServer();
