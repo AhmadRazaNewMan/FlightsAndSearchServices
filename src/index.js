a29a23794e6d0bca6478c969909095854950ca04
AhmadRazaNewMan <webdevolopmentbyar@gmail.com>
2024-01-12 15:07:55 +0500

    setup a basic .env file in ServerConfig.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const express  = require("express")
+const {PORT}  = require("./config/serverConfig")
 
 const startAndRundServer = () =>{
     // Create the object of the server and this is more important
     
     const app = express()
-    const PORT = 3000;
+    
 
     app.listen(PORT,()=>{
 
